4af29cbff2116de92a06214c43732abc7beca045
Louis Tran <loutran987@gmail.com>
2017-01-09 23:39:08 -0800

    Added a null reference to Payroll.java

--- examples/payroll/Payroll.java
+++ examples/payroll/Payroll.java
@@ -2,6 +2,9 @@ package payroll;
 
 public class Payroll {
 	public static void main(String[] args) {
+		/*
+		 * Part 1
+		 *
 		Address a1 = new Address("123", "South Main Street", "Cleveland", "OH", "12345");
 		Address a2 = new Address("123", "North Main Street", "Willoughby", "OH", "54321");
 		Employee ee1 = new Employee("Peggy", "Fisher", "717-555-1212", a1, 955123, 003, "Lecturer", 45000.00 );
@@ -31,5 +34,18 @@ public class Payroll {
 			System.out.println(a2);
 			System.out.println(a1);
 		}
+		*/
+		// Part 2
+		Address a1 = new Address("123", "South Main Street", "Cleveland", "OH", "12345");
+		Address a2 = new Address("123", "North Main Street", "Willoughby", "OH", "54321");
+		Address a3 = null;
+
+		Employee ee1 = new Employee("Peggy", "Fisher", "717-555-1212", a1, 955123, 003, "Lecturer", 45000.00 );
+		Employee ee2 = new Employee("Bob", "Fisher", "717-555-1212", a2, 955123, 003, "Lecturer", 45500.00 );
+		Employee ee3 = new Employee("Jane", "Smith", "510-555-1212", a3, 456789, 001, "Administrator", 55000 );
+
+		ee1.printEmployee();
+		ee2.printEmployee();
+		ee3.printEmployee();
 	}
 }
