a2bf8af6d9dbd202d4941d1818cf74233ae1f871
Louis Tran <loutran987@gmail.com>
2017-01-13 23:45:35 -0800

    Added Rectangle.java

---
+++ examples/geometry/Rectangle.java
@@ -0,0 +1,23 @@
+package geometry;
+import java.util.*;
+
+public class Rectangle extends Shape{
+
+	Scanner in = new Scanner(System.in);
+
+	public Rectangle() {
+		super("Rectangle");
+	}
+
+	public Rectangle(String name) {
+		super(name);
+	}
+
+	public double calculateArea() {
+		System.out.println("Enter the length and width of the rectangle: ");
+		double length = in.nextDouble();
+		double width = in.nextDouble();
+		return length * width;
+	}
+
+}
