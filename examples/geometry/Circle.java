575af4c354bc25c121ef100e7c3e16ce4a6f0a16
Louis Tran <loutran987@gmail.com>
2017-01-13 23:25:52 -0800

    Added Circle.java

---
+++ examples/geometry/Circle.java
@@ -0,0 +1,23 @@
+package geometry;
+import java.util.*
+
+public class Circle extends Shape{
+
+	Scanner in = new Scanner(System.in);
+
+	public Circle() {
+		super("Circle");
+	}
+
+	public Circle(String name) {
+		super(name);
+	}
+
+	public double calculateArea() {
+		System.out.println("Enter the radius of the circle: ");
+		double radius = in.nextDouble();
+		return Math.PI*Math.pow(radius, 2);
+	}
+
+
+}
