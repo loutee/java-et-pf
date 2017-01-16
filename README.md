44a1da20b1c922f76d2142a25462a3b058ef8b1b
Louis Tran <loutran987@gmail.com>
2017-01-15 23:37:01 -0800

    Added PizzaShop challenge and its requirements

--- README.md
+++ README.md
@@ -55,3 +55,6 @@ Files from following video tutorials by Peggy Fisher.
 * ##### KathiesCatering
 		Create an application to help manage Kathie's upcoming events
 		***Consider adding Peggy's solution features into program
+
+* ##### PizzaShop
+		Create an application for ordering pizza
---
+++ challenges/PizzaShop.java
@@ -0,0 +1,20 @@
+/* Challenge Objectives
+ * 
+ * 	Create an ordering application
+ * 	Requires the comparable interface
+ * 	This is for a pizza shop
+ * 	Two types of pizza: Regular and Sicilian
+ * 	Use compareTo based on size
+ * 	Instance data should be
+ * 		Size
+ * 		Type
+ * 		Toppings (ArrayList of strings)
+ * 		Cost
+ * 	Main program
+ * 		Customer name
+ * 		Type and size
+ * 		Number of toppings
+ * 			Up to 4 for regular
+ * 			Up to 2 for Sicilian
+ * 		Print out the total for the order
+ */
