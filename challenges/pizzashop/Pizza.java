304023a38f389f92814dd9b95bc8b1c0e73ba18d
Louis Tran <loutran987@gmail.com>
2017-01-16 23:36:29 -0800

    CompareTo implementation and methods in Pizza.java

--- challenges/pizzashop/Pizza.java
+++ challenges/pizzashop/Pizza.java
@@ -6,7 +6,7 @@ public abstract class Pizza implements Comparable {
 	private String type;
 	private ArrayList toppings;
 	private double cost;
-
+	
 	public Pizza(int size, String type, ArrayList toppings, double cost) {
 		this.size = size;
 		this.type = type;
@@ -14,9 +14,21 @@ public abstract class Pizza implements Comparable {
 		this.cost = cost;
 	}
 
+	public abstract addTopping();
+	public abstract calculateCost();
+
 	@Override
 	public int compareTo(Object p) {
-
+		Pizza pizza = (Pizza)p;
+		int size1 = Integer.parseInt(this.size);
+		int size2 = Integer.parseInt(this.size);
+		if (size1 > size2) {
+			return 1;
+		} else if (size1 == size2) {
+			return 0;
+		} else {
+			return -1;
+		}
 	}
 
 }
