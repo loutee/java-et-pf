a5faeab7a67b03718c7194c49066de4dd16e215e
Louis Tran <loutran987@gmail.com>
2017-01-16 23:52:49 -0800

    Added Regular.java, incomplete and rough

---
+++ challenges/pizzashop/Regular.java
@@ -0,0 +1,18 @@
+package pizzashop;
+import java.util.*;
+
+public class Regular extends Pizza {
+
+	public Regular(int size, String type, ArrayList toppings, double cost) {
+		super(size, type, toppings, cost);
+	}
+
+	public void addTopping(String topping) {
+
+	}
+
+	public double calculateCost() {
+
+	}
+
+}
