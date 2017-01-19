be2d634ec0620df37c59e514f610205c7f0f26f9
Louis Tran <loutran987@gmail.com>
2017-01-18 23:51:47 -0800

    Implemented Peggy's PizzaShop.java

--- challenges/pizzashop/PizzaShop.java
+++ challenges/pizzashop/PizzaShop.java
@@ -24,9 +24,68 @@ import java.util.*;
 
 public class PizzaShop {
 
-	public static void main(String[] args {
-		Pizza canadian = new Regular(12);
-		Pizza deepdish = new Sicilian(16);
-	}
+	public static void main(String[] args) {
 
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter customer name: ");
+		String name = in.nextLine();
+		System.out.println("What type of pizza would you like, regular"
+							+ " or Sicilian?"
+		String type = in.nextLine();
+		
+		if (type.equalsIgnoreCase("regular")) {
+			System.out.println("What size? S, M, L, X?");
+			String size = in.nextLine();
+			Regular pizza1 = new Regular();
+			switch (size) {
+				case "S":
+				case "s":
+					pizza1 = new Regular(12);
+					break;
+				case "M":
+				case "m":
+					pizza1 = new Regular(14);
+					break;
+				case "L":
+				case "l":
+					pizza1 = new Regular(16);
+					break;
+				case "X":
+				case "x":
+					pizza1 = new Regular(18);
+					break;
+				default:
+					break;
+			}
+
+			pizza1.getToppings();
+			pizza1.printString();
+
+		} else {
+			System.out.println("What size? S, M, L?");
+			String size = in.nextLine();
+			Sicilian pizza1 = new Sicilian();
+			
+			switch (size) {
+				case "S":
+				case "s":
+					pizza1 = new Sicilian(10);
+					break;
+				case "M":
+				case "m":
+					pizza1 = new Sicilian(12);
+					break;
+				case "L":
+				case "l":
+					pizza1 = new Sicilian(14);
+					break;
+				default:
+					break;
+			}
+
+			pizza1.getToppings();			
+			pizza1.printString();			
+
+		}
+	}
 }
