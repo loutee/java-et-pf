404a2a6db216a12abe5ed352b3d3db76a9603597
Louis Tran <loutran987@gmail.com>
2017-01-18 23:16:21 -0800

    Implemented Peggy's code solution for Pizza.java

--- challenges/pizzashop/Pizza.java
+++ challenges/pizzashop/Pizza.java
@@ -2,29 +2,48 @@ package pizzashop;
 
 public abstract class Pizza implements Comparable {
 
-	private int size;
-	private String type;
-	private ArrayList toppings;
-	private double cost;
+	private static final int small_diameter = 12;
+	private static final int med_diameter = 14;
+	private static final int lg_diameter = 16;
+	private static final int xlg_diameter = 18;
 
-	public Pizza(int size, String type, ArrayList toppings, double cost) {
-		this.size = size;
-		this.type = type;
-		this.toppings = toppings;
-		this.cost = cost;
+	public double[] price = {10.00, 12.00, 14.00, 16.00};
+	public double cost;
+	public String type;
+	public ArrayList<String> toppings = new ArrayList<String>();
+	int size;
+
+	public Pizza() {
+		type = "pizza";
 	}
 
-	public abstract void addTopping();
-	public abstract double calculateCost();
+	public Pizza(int size) {
+
+		switch(size) {
+			case small_diameter:
+				cost = price[0];
+				size = small_diameter;
+				break;
+			case med_diameter:
+				cost = price[1];
+				size = med_diameter;
+				break;
+			case lg_diameter:
+				cost = price[2];
+				size = xlg_diameter;
+				break;
+		}
+
+	}
+
+	public abstract void getToppings();
 
 	@Override
 	public int compareTo(Object p) {
-		Pizza pizza = (Pizza)p;
-		int size1 = Integer.parseInt(this.size);
-		int size2 = Integer.parseInt(this.size);
-		if (size1 > size2) {
+		Pizza other = (Pizza)p;
+		if (other.size > this.size) {
 			return 1;
-		} else if (size1 == size2) {
+		} else if (other.size == this.size) {
 			return 0;
 		} else {
 			return -1;
