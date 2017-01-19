90c14e762d5c3ff314b9893ee9e88a97f8b96751
Louis Tran <loutran987@gmail.com>
2017-01-18 23:41:12 -0800

    Implemented Peggy's solution of Sicilian.java

--- challenges/pizzashop/Sicilian.java
+++ challenges/pizzashop/Sicilian.java
@@ -3,19 +3,37 @@ import java.util.*;
 
 public class Sicilian extends Pizza {
 
-	public Sicilian(int size, String type, ArrayList toppings, double cost) {
-		super(size, type, toppings, cost);
-	}   
+	public static final int small_width = 10;
+	public static final int med_width = 12;
+	public static final int lg_width = 14;
 
-    public void addTopping(String topping) {
-		if (toppings.size() <= 2) {
+	public Sicilian(int size) {
+		super(size);
+		this.type = "Sicilian";
+	}
+
+	public double getCost() {
+		return this.cost;
+	}
+
+    public void getToppings() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("How many toppings would you like?");
+		int numToppings = in.nextInt();
+		while(numToppings > 2) {
+			System.out.println("Sorry, you are limited to two.");
+			System.out.println("How many toppings would you like?");
+			numToppings = in.nextInt();
+		}
+
+		in.nextLine();
+		
+		for(int i = 0; i < numToppings; i++) {
+			System.out.println("Enter topping: ");
+			String topping = in.nextLine();
 			toppings.add(topping);
-		} else {
-			System.out.println("The maximum number of toppings has been reached");
-		}   
+			this.cost += 2.50;
+		}
 
-    public double calculateCost() {
-	    return 5 + size + 2*toppings.size();
-	}  	
-
+	}  
 }
