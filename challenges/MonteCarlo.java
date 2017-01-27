f71a38bc995779a3906b49dcbe77ad1da9614913
Louis Tran <loutran987@gmail.com>
2017-01-26 18:54:15 -0800

    Added MonteCarlo challenge solution

--- challenges/MonteCarlo.java
+++ challenges/MonteCarlo.java
@@ -9,7 +9,7 @@
  *			Create unique variables for blue/white
  *			Add variables to count attempts/success
  *			Loop simulation
- *			Create an array to simulate te bowl
+ *			Create an array to simulate the bowl
  *			Create an array to hold marbles selected
  *			Create a loop to draw three
  *			Inside, randomly choose one
@@ -25,7 +25,56 @@
 public class MonteCarlo {
 
 	public static void main(String[] args) {
-
+		
+		System.out.println("Probability of drawing 3 marbles of the same colour:");
+		System.out.println("100 trials: " + marbleSim(100));
+		System.out.println("100 trials: " + marbleSim(1000));
+		System.out.println("100 trials: " + marbleSim(10000));
 
 	}
+
+	public static String marbleSim(int trials) {
+
+		final int B = 1;
+		final int W = 2;
+		
+		double number = 0.0;
+		double result = 0.0;
+
+		for (int t=0; t<trials; t++) {
+			
+			result++;
+
+			int[] bowl = { B, B, B, W, W, W };
+			int[] drawMarbles = new int[3];
+
+			for (int draw = 0; draw < 3; draw++) {
+
+				int index = (int) (Math.random() * bowl.length);
+				drawMarbles[draw] = bowl[index];
+				int[] newBowl = new int[bowl.length - 1];
+				int j = 0;
+
+				for (int i=0; i<bowl.length; i++) {
+					if (i == index) {
+						continue;
+					}
+					newBowl[j] = bowl[i];
+					j++;
+				}
+				bowl = newBowl;
+			}
+			
+			if (drawMarbles[0] == drawMarbles[1] && drawMarbles[1] == drawMarbles[2]) {
+				number++;
+				// System.out.println("winner");
+			}
+			//System.out.println(Arrays.toString(drawMarbles));
+		}
+
+		return "Results: " + (number / result);
+
+	}
+		
+
 }
